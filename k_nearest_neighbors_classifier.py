776c75c3263812c7fafe74477ba35d1324f02fb2
yura95st <yura95st@gmail.com>
2017-02-25 01:02:02 +0200

    Add KNN classifier.

---
+++ k_nearest_neighbors_classifier.py
@@ -0,0 +1,42 @@
+import operator
+from util import Util
+
+class KNearestNeighborsClassifier:
+
+    def __init__(self, training_set, k):
+        self.training_set = training_set
+        self.k = k
+
+    def _get_neighbors(self, test_item):
+        distances = []
+
+        for item in self.training_set:
+            dist = Util.euclidean_distance(item[:-1], test_item[:-1])
+
+            distances.append((item, dist))
+
+        distances.sort(key=operator.itemgetter(1))
+
+        return [distances[i][0] for i in range(self.k)]
+
+    def _get_mode_class_number(self, neighbors):
+        classes = {}
+
+        for neighbor in neighbors:
+            class_num = neighbor[-1]
+
+            if class_num in classes:
+                classes[class_num] += 1
+            else:
+                classes[class_num] = 1
+
+        classes = sorted(classes, key=classes.get, reverse=True)
+
+        return next(iter(classes))
+
+    def classify(self, item):
+        neighbors = self._get_neighbors(item)
+
+        class_num = self._get_mode_class_number(neighbors)
+
+        return class_num
--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-	pass
+from util import Util
+from k_nearest_neighbors_classifier import KNearestNeighborsClassifier
+from nearest_neighbors_clustering import NearestNeighborsClustering
+
+
+def main_classifying(k=3, training_set_ratio=0.2):
+    dataset = Util.load_dataset('data/iris.data')
+
+    training_set, test_set = Util.split_dataset(dataset, training_set_ratio)
+
+    classifier = KNearestNeighborsClassifier(training_set, k)
+
+    result = [(item, classifier.classify(item)) for item in test_set]
+
+    for item in result:
+        print(item)
+    print()
+    print('Accuracy: {}'.format(Util.get_accuracy(result)))
+
+
+def main_clustering(clusters_num=3):
+    dataset = Util.load_dataset('data/iris.data')
+
+    clustering = NearestNeighborsClustering()
+
+    clusters = clustering.perform(dataset, clusters_num)
+
+    for cluster in clusters:
+        for item in cluster:
+            print(item)
+        print()
 
 
 if __name__ == '__main__':
-    main()
+    main_classifying()
+    # main_clustering()
