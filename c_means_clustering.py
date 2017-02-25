f6f83227c8a1c9448a3689fb5968e4250de806ac
yura95st <yura95st@gmail.com>
2017-02-25 02:33:18 +0200

    Add c-means clustering algorithm.

---
+++ c_means_clustering.py
@@ -0,0 +1,70 @@
+import random
+from util import Util
+
+
+class CMeansClustering:
+
+    def __init__(self, alpha):
+        self.alpha = alpha
+
+    def _get_cluster_id(self, cluster_centers, item):
+        cluster_id = -1
+        min_distance = float('inf')
+
+        for i, center in enumerate(cluster_centers):
+            dist = Util.euclidean_distance(
+                Util.remove_class_id(center), Util.remove_class_id(item))
+
+            if dist < min_distance:
+                min_distance = dist
+                cluster_id = i
+
+        return cluster_id
+
+    def _get_cluster_center(self, cluster):
+        center = cluster[0][:]
+
+        for item in cluster[1:]:
+            for i, val in enumerate(item):
+                center[i] += val
+
+        length = len(cluster)
+
+        return [val / length for val in center]
+
+    def _are_cluster_centers_changed(self, cluster_centers, new_cluster_centers):
+        for i, center in enumerate(cluster_centers):
+            dist = Util.euclidean_distance(
+                Util.remove_class_id(center), Util.remove_class_id(new_cluster_centers[i]))
+
+            if dist > self.alpha:
+                return True
+
+        return False
+
+    def perform(self, dataset, n):
+        indexes = [random.randint(0, len(dataset) - 1) for _ in range(n)]
+
+        cluster_centers = [dataset[i] for i in indexes]
+        clusters = {}
+
+        while True:
+            clusters.clear()
+
+            for item in dataset[:]:
+                cluster_id = self._get_cluster_id(cluster_centers, item)
+
+                if cluster_id in clusters:
+                    clusters[cluster_id].append(item)
+                else:
+                    clusters[cluster_id] = [item]
+
+            new_cluster_centers = [self._get_cluster_center(
+                cluster) for cluster in clusters.values()]
+
+            if not self._are_cluster_centers_changed(cluster_centers, new_cluster_centers):
+                break
+
+            cluster_centers = new_cluster_centers
+
+        return clusters.values()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from util import Util
 from k_nearest_neighbors_classifier import KNearestNeighborsClassifier
 from nearest_neighbors_clustering import NearestNeighborsClustering
+from c_means_clustering import CMeansClustering
 
 
 def main_classifying(k=3, training_set_ratio=0.2):
@@ -18,19 +19,21 @@ def main_classifying(k=3, training_set_ratio=0.2):
     print('Accuracy: {}'.format(Util.get_accuracy(result)))
 
 
-def main_clustering(clusters_num=3):
+def main_clustering(clusters_num=3, alpha=0.005):
     dataset = Util.load_dataset('data/iris.data')
 
-    clustering = NearestNeighborsClustering()
+    # clusterings = [NearestNeighborsClustering(), CMeansClustering(alpha)]
+    clusterings = [CMeansClustering(alpha)]
 
-    clusters = clustering.perform(dataset, clusters_num)
+    for clustering in clusterings:
+        clusters = clustering.perform(dataset, clusters_num)
 
-    for cluster in clusters:
-        for item in cluster:
-            print(item)
-        print()
+        for cluster in clusters:
+            for item in cluster:
+                print(item)
+            print()
 
 
 if __name__ == '__main__':
-    main_classifying()
-    # main_clustering()
+    # main_classifying()
+    main_clustering()
